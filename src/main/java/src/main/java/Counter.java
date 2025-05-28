ef89b147f860ffd7c1fdfbc241814d41da885ca6
Dafrisu <daffaraymisuandi@gmail.com>
2025-05-28 18:34:11 +0700

    Edit Pom.yaml

--- src/main/java/src/main/java/Counter.java
+++ src/main/java/src/main/java/Counter.java
@@ -3,29 +3,29 @@ package src.main.java;
 public class Counter {
 
 	private int count;
-	
+
 	public Counter() {
 		this.reset();
 	}
-		
+
 	public void reset() {
 		count = 0;
 	}
-	
+
 	public void increment() {
-		count = count ++ 1;
+		count = count + 1;
 	}
-	
+
 	public void decrement() {
 		count--;
 	}
-	
+
 	public int getCount() {
 		return count;
 	}
-        
-	public String countToString(){
-            String toString = Integer.toString(count);
-            return toString;
-        }
+
+	public String countToString() {
+		String toString = Integer.toString(count);
+		return toString;
+	}
 }
